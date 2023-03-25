cd559e7a712856b7a970098307fe3cc6983eb041
Vladyslav Ohirenko <aguretsvlad@gmail.com>
2023-03-25 22:57:19 +0200

    chore: init

---
+++ src/secrets.example.h
@@ -0,0 +1,9 @@
+#ifndef SECRETS_H
+#define SECRETS_H
+
+const char* WIFI_SSID = "wifi name";
+const char* WIFI_PASSWORD = "wifi pass";
+const int TELEGRAM_USER_ID = 123456789; // Your Telegram User Id
+const char* TELEGRAM_TOKEN = "telegram_bot_token";
+
+#endif
