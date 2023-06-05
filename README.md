4ab30ffc79d8b0c32eb4d41725bfebebb31ced1b
Vladyslav Ohirenko <aguretsvlad@gmail.com>
2023-06-05 21:41:01 +0300

    feat: add readme

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
-Treasury locker based on ESP32 module, Servo and CTBot (Telegram)
+# Treasury locker based on ESP32 module, Servo and [CTBot](https://github.com/shurillu/CTBot)
 
-<!-- WIP -->
+This is a pet project to deal with ESP32 chip and IoT.
+
+Main motivation is to build a small locker box with an ability to controll it over Telegram bot.
+Current implementation allows to open box several times per each week (2 times by default).
+
+To achieve rusults I took the following components:
+* [ESP32](https://www.espressif.com/en/products/hardware/esp32/overview) module
+* [Servo](https://www.adafruit.com/product/169) motor
+
+Pre-conditions:
+- Build your own locker. I modeled and printed a casket with an ability to integrate ESP32Cam module & SG92R
+- [Setup Telegramm bot and get API token](https://core.telegram.org/bots#how-do-i-create-a-bot)
+- Create `./src/secrets.h` files, copy content from `./secrets.example.h` and update to your desired values
+- Update `./src/constants.h` to your desired values. Check definitions inside file
+- Burn ESP32
+- Find your bot in Telegramm app and start chatting
+
+As result you will be able to open your cascet `n` times per week (initially week starts on Friday and ends on Thursday, you can play with configs in `./src/constants.h`)
--- src/constants.h
+++ src/constants.h
@@ -1,21 +1,25 @@
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
 
+// NOT editable
 const char* NTP_SERVER = "pool.ntp.org";
 const char* OPEN_COMMAND_TEXT = "/open";
 const char* WHEN_COMMAND_TEXT = "/when";
 const char* AMOUNT_COMMAND_TEXT = "/amount";
+
+const int NUMBER_OF_CURRENT_WEEK_ADDRESS = 0;
 const int CURRENT_WEEK_OPENINGS_COUNT_ADDRESS = 2;
 const int EEPROM_SIZE = 512;
-const int FINAL_SERVO_ANGLE = 50;
 const int FLASH_LED_MAX_INTENCIVITY = 5;
 const int FLASH_LED_PIN = 4;
 const int FLASH_PWM_CHANNEL = 0;
-const int INITIAL_SERVO_ANGLE = 160;
-const int MAX_MESSAGE_AGE = 60; // in seconds
-const int MAX_OPENINGS_PER_WEEK = 2;
-const int NUMBER_OF_CURRENT_WEEK_ADDRESS = 0;
-const int SERVO_PIN = 12;
-const int DAY_OF_WEEK_TO_RESET = 5; // Number of week day
+
+// Editable
+const int SERVO_PIN = 12; // Analog pin number of servo angle
+const int INITIAL_SERVO_ANGLE = 160; // Initial angle of servo motor (it will move to this angle every start)
+const int FINAL_SERVO_ANGLE = 50;  // Final angle of servo motor (it will move to this angle after catching of open command)
+const int MAX_MESSAGE_AGE = 60; // TTL of every message catched by Telegramm bot. If message is older than this value bot will ignore this command
+const int MAX_OPENINGS_PER_WEEK = 2; // Max amount of opens per week
+const int DAY_OF_WEEK_TO_RESET = 5; // Number of week start day. 1-7, 5 is a Friday so week in this case starts from Friday and ends at Thursday
 
 #endif
